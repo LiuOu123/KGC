848c31b0e9d69c89f39c526518dbb23b71e88955
sunkuo1106 <sunkuo1106@163.com>
2020-09-25 19:42:41 +0800

    test2

--- KGC/src/main/java/com/kgc/service/impl/asd.java
+++ KGC/src/main/java/com/kgc/service/impl/asd.java
@@ -4,5 +4,6 @@ public class asd {
     public static void main(String[] args) {
         System.out.println("刘鸥");
         System.out.println("孙阔");
+        System.out.println("好运来呀 好运来");
     }
 }
