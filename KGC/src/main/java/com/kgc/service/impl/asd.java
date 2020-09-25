a66cba5c9b7bf60b53fd7c425b8f548e5bed0c08
liuou <1367423614@qq.com>
2020-09-25 20:04:04 +0800

    impl测试2

--- KGC/src/main/java/com/kgc/service/impl/asd.java
+++ KGC/src/main/java/com/kgc/service/impl/asd.java
@@ -1,1 +1,11 @@
-package com.kgc.service.impl;public class asd {    public static void main(String[] args) {        System.out.println("刘鸥");        System.out.println("孙阔");        System.out.println("最近好倒霉");    }}
+package com.kgc.service.impl;
+
+public class asd {
+    public static void main(String[] args) {
+        System.out.println("刘鸥");
+        System.out.println("孙阔");
+        System.out.println("好运来呀 好运来");
+        System.out.println("孙阔洗白白");
+    }
+}
+
